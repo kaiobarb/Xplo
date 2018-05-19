6f1002622d7ac9757dbf4613032c8dc2877650cf
Kaio <kaiobarb@gmail.com>
2018-05-19 16:13:21 -0700

    created web2py app, added more image folder and more logo dimensions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-![alt text](/xplo_logo_border.png?raw=true "Title")
+![alt text](/images/xplo_logo_border256x759.png?raw=true "Title")
+
 The webapp that exposes the real story of a place.
--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$993bfab86957b515$4987c87970c086c1c41cf542b6b3582b46a9d3a0"
+password="pbkdf2(1000,20,sha512)$aef0ff63d1ffadd9$f0a689ef237559675fe420b1dedbfc9aab2a0f98"
