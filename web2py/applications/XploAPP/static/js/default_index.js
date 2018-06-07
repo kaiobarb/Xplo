9ec11d37248ed93f0f39149423c66f7cd05080c6
Pranav Salunke <pranav.salunke@gmail.com>
2018-06-07 15:27:19 -0700

    can click markers. doesnt do anything except console log

--- web2py/applications/XploAPP/static/js/default_index.js
+++ web2py/applications/XploAPP/static/js/default_index.js
@@ -1,13 +1,13 @@
 // This is the js for the default/index.html view.
 // 
-var app = function() {
+var app = function () {
 
     var self = {};
 
     Vue.config.silent = false; // show all warnings
 
     // Extends an array
-    self.extend = function(a, b) {
+    self.extend = function (a, b) {
         for (var i = 0; i < b.length; i++) {
             a.push(b[i]);
         }
@@ -15,40 +15,54 @@ var app = function() {
 
     // args: a location consisting of a latitude and longitude
     // returns: a marker object
-    self.placeMarker = function placeMarker(location) {
+    self.placeMarker = function placeMarker(location, title) {
         var marker = new google.maps.Marker({
-            position: location, 
-            map: map
+            position: location,
+            map: map,
+            title: title,
         });
         return marker
     }
 
-    self.add_story_button = function() {
-        console.log("before: ",self.is_adding);
-        self.is_adding = !self.is_adding;
-        console.log("after: ",self.is_adding);
-        self.add_story();
+    self.marker_clicked = function (title) {
+        console.log("clicked marker: " + title)
     }
 
-    self.add_story = function() {
+    self.add_story_button = function () {
+        console.log("before: ", self.is_adding);
+        self.is_adding = !self.is_adding;
+        console.log("after: ", self.is_adding);
+        self.add_story("placeholder title: " + new Date().getTime());
+    }
+
+    self.add_story = function (storyTitle) {
         var marker = null;
-        console.log("pre: ",self.is_adding);
+        console.log("pre: ", self.is_adding);
         if (self.is_adding) {
-            listener = map.addListener('click', function(event) {
-                marker = self.placeMarker(event.latLng);
+
+            listener = map.addListener('click', function (event) {
+                marker = self.placeMarker(event.latLng, storyTitle);
+                // marker.addListener('click', self.marker_clicked(marker.title))
+                marker.addListener('click', function () {
+                    self.marker_clicked(marker.title)
+                })
+
                 $.post(add_story_url,
-                {
-                    lat: marker.position.lat,
-                    lng: marker.position.lng,
-                },
-                function(data) {
-                    google.maps.event.removeListener(listener);
-                    self.is_adding = !self.is_adding;
-                    console.log("done: ",self.is_adding);
-                }
-            )
+                    {
+                        lat: marker.position.lat,
+                        lng: marker.position.lng,
+                    },
+                    function (data) {
+                        google.maps.event.removeListener(listener);
+                        self.is_adding = !self.is_adding;
+                        console.log("done: ", self.is_adding);
+                    }
+                )
             });
-        }   
+
+
+
+        }
     };
 
     // Complete as needed.
@@ -66,6 +80,7 @@ var app = function() {
         methods: {
             add_story: self.add_story,
             add_story_button: self.add_story_button,
+            marker_clicked: self.marker_clicked,
         }
 
     });
@@ -76,4 +91,4 @@ var APP = null;
 
 // This will make everything accessible from the js console;
 // for instance, self.x above would be accessible as APP.x
-jQuery(function(){APP = app();});
+jQuery(function () { APP = app(); });
